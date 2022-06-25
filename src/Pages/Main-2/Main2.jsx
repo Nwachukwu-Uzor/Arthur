be7b048410ab33ecfcc81b9d0017b64ae03051f7
Uzor Nwachukwu <uzor1997@gmail.com>
2022-06-25 12:21:55 +0100

    adding auth form

--- src/Pages/Main-2/Main2.jsx
+++ src/Pages/Main-2/Main2.jsx
@@ -1,23 +1,125 @@
 import React from "react";
 import styles from "./main2.module.css";
 
+import { useState } from "react";
+
 import Logo from "../../assets/Logo.png";
 import star from "../../assets/svg/Star.svg";
 
+import leftBorder from "../../assets/left-border.png";
+import rightBorder from "../../assets/right-border.png";
+
+import facebook from "../../assets/svg/Facebook.svg";
+import ios from "../../assets/svg/ios.svg";
+import google from "../../assets/svg/google.svg";
+
 const Main2 = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const Input = ({ type, placeholder }) => (
+    <div className="my-2 w-full">
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="outline-none bg-gray-100 px-1 py-2 rounded-lg w-full focus:ring-4 
+              focus:ring-blue-300 focus:ring-opacity-50"
+      />
+    </div>
+  );
+
+  const FileShareForm = () => (
+    <form
+      className={`${styles.form} flex items-center justify-stretch flex-col`}
+    >
+      <div className={`flex items-center`}>
+        <i class="fa-light fa-folder-medical mr-4 text-2xl text-gray-200"></i>
+        <div>
+          <h4 className="text-2xl font-bold ">Upload File</h4>
+          <p className="underline hover:text-opacity-50 cursor-pointer my-1">
+            Or select a folder
+          </p>
+        </div>
+      </div>
+      <Input placeholder={"Title"} type={"text"} />
+      <Input placeholder={"Receiver Email"} type={"text"} />
+      <Input placeholder={"Message"} type={"text"} />
+      <button
+        className="bg-gray-900 text-white w-full p-2 
+              my-3 rounded-md 
+              "
+      >
+        Get a link
+      </button>
+    </form>
+  );
+
+  const Border = ({ src, alt }) => (
+    <div>
+      <img src={src} alt={alt} className="h-0.5 block w-12" />
+    </div>
+  );
+
+  const AuthLogo = ({ src, alt }) => (
+    <div className="p-2 rounded-3xl bg-gray-200 cursor-pointer hover:opacity-50">
+      <img src={src} alt={alt} className="h-4" />
+    </div>
+  );
+
+  const AuthForm = () => (
+    <form
+      className={`${styles.form} flex items-center justify-stretch flex-col`}
+    >
+      <h4 className="text-2xl text-center md:text-3xl font-bold">
+        Welcome Back
+      </h4>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam maecenas
+        mi non sed ut odio. Non, justo, sed facilisi et.{" "}
+      </p>
+      <Input type={"text"} placeholder="Username , Email & Phone Number" />
+      <Input type={"password"} placeholder="Password" />
+      <p className="text-right my-2 w-full">
+        <a href="#" className="underline font-medium">
+          Forgot Password ?
+        </a>
+      </p>
+      <button
+        className="bg-blue-400 hover:opacity-50 text-white w-full p-2 
+              my-3 rounded-md 
+              "
+      >
+        Sign in
+      </button>
+      <div className="my-2 flex justify-between items-center h-10 gap-x-1">
+        <Border src={leftBorder} alt={"left border"} />
+        <p className="text-xs">Or Sign up With</p>
+        <Border src={rightBorder} alt={"left border"} />
+      </div>
+      <div className="flex justify-between align-center w-2/5 mx-auto">
+        <AuthLogo src={google} alt="google logo" />
+        <AuthLogo src={facebook} alt="facebook logo" />
+        <AuthLogo src={ios} alt="ios logo" />
+      </div>
+    </form>
+  );
+
   return (
     <section className={styles.main2}>
-      <div className="flex flex-col justify-start px-5 items-start h-full">
+      <div
+        className={`flex flex-col justify-start px-5 items-start min-h-screen ${
+          !isAuth && styles.blue
+        }`}
+      >
         <img
           src={Logo}
           alt="share logo"
           className={`${styles.logo} h-4 md:h-6 mt-2`}
         />
         <div className={`self-center m-auto ${styles.main2Text}`}>
-          <h2 className="text-2xl md:text-3xl font-bold my-3">
+          <h2 className="text-2xl md:text-5xl font-bold my-3">
             File Sharing Made{" "}
-            <span className="text-blue-300">
-              EASY <img src={star} alt="" className="h-5 inline-block" />
+            <span className="text-blue-300 flex items-center">
+              EASY <img src={star} alt="" className="ml-2 h-5 inline-block" />
             </span>
           </h2>
           <p>
@@ -27,51 +129,12 @@ const Main2 = () => {
           </p>
         </div>
       </div>
-      <div className={`${styles.blue} flex justify-center items-center`}>
-        <form
-          className={`${styles.form} flex items-center justify-stretch flex-col`}
-        >
-          <div className={`flex items-center`}>
-            <i class="fa-light fa-folder-medical mr-4 text-2xl text-gray-200"></i>
-            <div>
-              <h4 className="text-2xl font-bold ">Upload File</h4>
-              <p className="underline hover:text-opacity-50 cursor-pointer my-1">
-                Or select a folder
-              </p>
-            </div>
-          </div>
-          <div className="my-2 w-full">
-            <input
-              type="text"
-              placeholder="Title"
-              className="outline-none bg-gray-100 px-1 py-2 rounded-lg w-full focus:ring-4 
-              focus:ring-blue-300 focus:ring-opacity-50"
-            />
-          </div>
-          <div className="my-2 w-full">
-            <input
-              type="text"
-              placeholder="Receiver Email"
-              className="outline-none bg-gray-100 px-1 py-2 rounded-lg w-full focus:ring-4 
-              focus:ring-blue-300 focus:ring-opacity-50"
-            />
-          </div>
-          <div className="my-2 w-full">
-            <input
-              type="text"
-              placeholder="Message"
-              className="outline-none bg-gray-100 px-1 py-2 rounded-lg w-full focus:ring-4 
-              focus:ring-blue-300 focus:ring-opacity-50"
-            />
-          </div>
-          <button
-            className="bg-gray-900 text-white w-full p-2 
-              my-2 rounded-md 
-              "
-          >
-            Get a link
-          </button>
-        </form>
+      <div
+        className={`${
+          isAuth && styles.blue
+        } flex justify-center items-center min-h-screen`}
+      >
+        {isAuth ? <FileShareForm /> : <AuthForm />}
       </div>
     </section>
   );
